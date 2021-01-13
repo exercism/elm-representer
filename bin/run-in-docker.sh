715e513767a1757a29a4164587f6c1e9cd4330f8
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-01-13 13:10:12 +0100

    Update container for representer (#5)
    The 3 main differences are:
    1. the build is multi-stage for a lighter runner container
    2. it uses `elm-strip-comments` instead of `cloc --strip-comments`
    3. the container isn't read-only anymore, making parts of `run.sh` simpler
    
    * Update container for representer
    
    * Temporary CI on PR
    
    * Reset docker action on push
    
    * Fix inconsistent parameter names
    
    Co-authored-by: ceddlyburge <ceddlyburge@gmail.com>

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -12,17 +12,18 @@
 # $3: **RELATIVE** path to output directory (with trailing slash)
 
 # Output:
-# Writes the normalised code to the passed-in output directory.
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/automated-tests/blob/master/docs/interface.md
 
 # Example:
-# bin/run-in-docker.sh two-fer path/to/two-fer/solution/folder/ path/to/output/directory/
+# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder/ ./relative/path/to/output/directory/
 
 set -e # Make script exit when a command fail.
 set -u # Exit on usage of undeclared variable.
 # set -x # Trace what gets executed.
 set -o pipefail # Catch failures in pipes.
 
-USAGE="bin/run-in-docker.sh <exercise-slug> <exercise_directory> <output_directory>"
+USAGE="bin/run-in-docker.sh <exercise-slug> ./relative/path/to/solution/folder/ ./relative/path/to/output/directory/"
 
 # If arguments not provided, print usage and exit
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
@@ -32,11 +33,16 @@ fi
 
 SLUG="$1"
 # Retrieve absolute normalized paths
-INPUT=$(readlink -f $2)
-OUTPUT=$(readlink -f $3)
+INPUT_DIR=$(readlink -f $2)
+OUTPUT_DIR=$(readlink -f $3)
 
 # build docker image
+# docker build --rm --no-cache -t elm-representer .
 docker build -t elm-representer .
 
 # run image passing the arguments
-docker run --network none --read-only --mount type=bind,src=$INPUT,dst=/solution --mount type=bind,src=$OUTPUT,dst=/output elm-representer $SLUG /solution /output
+mkdir -p "$OUTPUT_DIR"
+docker run --network none \
+    --mount type=bind,src=$INPUT_DIR,dst=/solution \
+    --mount type=bind,src=$OUTPUT_DIR,dst=/output \
+    elm-representer $SLUG /solution/ /output/
